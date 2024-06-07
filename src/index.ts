504b8ef64b452b52bc4e15c1e7dca885dc432bc8
JuhoJoona <jjoona06@gmail.com>
2024-06-07 20:14:09 +0300

    start over

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,9 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-const dbUrl = process.env.DATABASE_URL || 'mongodb://localhost:27017/database';
 
-mongoose.connect(dbUrl)
-  .then(() => console.log('🛢️  MongoDB connected'))
-  .catch(err => console.error('⚠️  Failed to connect to MongoDB:', err));
 
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use('/auth', auth);
+
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
