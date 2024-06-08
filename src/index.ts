974e30595d909848d5ea62be6b1be83a23788e4e
JuhoJoona <jjoona06@gmail.com>
2024-06-08 11:10:02 +0300

    postgres implementation

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,42 @@
 import express, { Express } from 'express';
+import { DataSource } from 'typeorm';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-import auth from './routes/authRoutes';
 import cors from 'cors';
-import mongoose from 'mongoose';
+import { User } from './models/user';
 
 dotenv.config();
 
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'your_username',
+  password: 'your_password',
+  database: 'your_database',
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+});
+
+AppDataSource.initialize()
+  .then(() => {
+      console.log("Data Source has been initialized!");
+      // Here you can start to work with your entities
+  })
+  .catch((error) => console.log("Error during Data Source initialization", error));
+
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json())
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
 
 
 
